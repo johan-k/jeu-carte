768e0625bef04a609b3529d2808d49be53399119
Johan Kerdekachian <johan.kerdekachian@gmail.com>
2024-04-16 20:31:41 +0200

    Initial commit

---
+++ src/main/java/com/example/jeucarte/JeuCarteApplication.java
@@ -0,0 +1,13 @@
+package com.example.jeucarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeuCarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeuCarteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jeucarte/JeuCarteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jeucarte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeuCarteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
